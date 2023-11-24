8042fe4a75a282d0ef42c04d431826b30b410fdd
ashwin2023paralikar <121968470+ashwin2023paralikar@users.noreply.github.com>
2023-11-24 21:09:20 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,56 @@
+//Code for JavaScript
+function fun1()
+{
+    document.getElementById("label5").innerHTML="Name:-";
+document.getElementById("label6").innerHTML="Comment:-";
+x=document.getElementById("textbox1").value;
+document.getElementById("para1").innerHTML=x;
+y=document.getElementById("textbox2").value;
+document.getElementById("para2").innerHTML=y;
+}
+function fun2()
+{
+    document.getElementById("label3").innerHTML="Name:-";
+document.getElementById("label4").innerHTML="Comment:-";
+    
+    z=document.getElementById("textbox3").value;
+    document.getElementById("para3").innerHTML=z;
+    a=document.getElementById("textbox4").value;
+    document.getElementById("para4").innerHTML=a;
+}
+function fun3()
+{
+    document.getElementById("label1").innerHTML="Name:-";
+    document.getElementById("label2").innerHTML="Comment:-";
+    b=document.getElementById("textbox5").value;
+    document.getElementById("para5").innerHTML=b;
+    c=document.getElementById("textbox6").value;
+    document.getElementById("para6").innerHTML=c;
+}
+function fun4()
+{
+    document.getElementById("label7").innerHTML="Name:-";
+    document.getElementById("label8").innerHTML="Comment:-";
+    d=document.getElementById("textbox7").value;
+    document.getElementById("para7").innerHTML=d;
+    e=document.getElementById("textbox8").value;
+    document.getElementById("para8").innerHTML=e;
+}
+function fun5()
+{
+    document.getElementById("label9").innerHTML="Name:-";
+    document.getElementById("label10").innerHTML="Comment:-";
+    d=document.getElementById("textbox9").value;
+    document.getElementById("para9").innerHTML=d;
+    e=document.getElementById("textbox10").value;
+    document.getElementById("para10").innerHTML=e;
+}
+function fun6()
+{
+    document.getElementById("label11").innerHTML="Name:-";
+    document.getElementById("label12").innerHTML="Comment:-";
+    d=document.getElementById("textbox11").value;
+    document.getElementById("para11").innerHTML=d;
+    e=document.getElementById("textbox12").value;
+    document.getElementById("para12").innerHTML=e;
+}
